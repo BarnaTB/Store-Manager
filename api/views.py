a2bc9328a85a2c4c5f6128c9d5c67b2614ac2711
BarnaTB <barnabastb2@gmail.com>
2018-11-01 11:46:29 +0300

    Catch any uncaught exceptions

--- api/views.py
+++ api/views.py
@@ -136,6 +136,10 @@ def add_product():
         return jsonify({
             'message': 'The unit price and quantity must be numbers!'
         }), 400
+    except Exception:
+        return jsonify({
+            'message': 'Something went wrong with your inputs!'
+        }), 400
 
 
 @blueprint.route('/sales', methods=['POST'])
